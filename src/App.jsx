757fa2664dbe111698fa0f391e9a5191a72bd2c9
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-05-25 20:30:53 +0800

    done layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
 import Button from "./component/Button.jsx"
+import { useState } from "react";
+import './App.css'
 
 export default function App () {
+  const [input, setInput] = useState("")
 
-  function clicked (){
-    alert("button clicked!")
+  function clicked (number){
+    setInput((i) => i + String(number))
+  }
+
+  function delete_input (){
+    setInput((i) => i.split("").slice(0, i.length - 1).join(""))
   }
 
   return (
     <div>
-        <Button title="7" onClick={clicked}/>
-        <Button title="8" onClick={clicked}/>
-        <Button title="9" onClick={clicked}/>
-        <Button title="÷" onClick={clicked}/>
-        
-        <Button title="4" onClick={clicked}/>
-        <Button title="5" onClick={clicked}/>
-        <Button title="6" onClick={clicked}/>
-        <Button title="x" onClick={clicked}/>
+      <nav className="answer-container">
+        <span className="answer">{input}</span>
+      </nav>
+      <main className="button-container">
+        <div className="grid-button-container">
+          <Button title="7" onClick={() => clicked(7)}/>
+          <Button title="8" onClick={() => clicked(8)}/>
+          <Button title="9" onClick={() => clicked(9)}/>
+          <Button title="÷" onClick={() => clicked("÷")}/>
 
-        <Button title="1" onClick={clicked}/>
-        <Button title="2" onClick={clicked}/>
-        <Button title="3" onClick={clicked}/>
-        <Button title="-" onClick={clicked}/>
+          <Button title="4" onClick={() => clicked(4)}/>
+          <Button title="5" onClick={() => clicked(5)}/>
+          <Button title="6" onClick={() => clicked(6)}/>
+          <Button title="x" onClick={() => clicked("x")}/>
 
-        <Button title="0" onClick={clicked}/>
-        <Button title="." onClick={clicked}/>
-        <Button title="=" onClick={clicked}/>
-        <Button title="+" onClick={clicked}/>
+          <Button title="1" onClick={() => clicked(1)}/>
+          <Button title="2" onClick={() => clicked(2)}/>
+          <Button title="3" onClick={() => clicked(3)}/>
+          <Button title="-" onClick={() => clicked("-")}/>
+
+          <Button title="0" onClick={() => clicked(0)}/>
+          <Button title="." onClick={() => clicked(".")}/>
+          <Button title="Del" onClick={delete_input}/>
+          <Button title="+" onClick={() => clicked("+")}/>
+        </div>
+        <div className="equal-container">
+          <Button title="=" onClick={() => clicked("=")} className="equal"/>
+        </div>
+      </main>
     </div>
   );
 }
--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -2,7 +2,7 @@ import "./Button.css"
 
 export default function Button (prop) {
     return (
-        <button onClick={prop.onClick}>
+        <button onClick={prop.onClick} className={prop.className}>
             {prop.title}
         </button>
     )
