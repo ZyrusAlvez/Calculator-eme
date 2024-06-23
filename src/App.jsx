5d97b2efb0fc02ffd4612eed1e115c2b22e6dc3c
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-06-23 15:08:28 +0800

    updated grid

--- src/App.jsx
+++ src/App.jsx
@@ -18,36 +18,32 @@ export default function App () {
   }
 
   return (
-    <div>
+    <div className="main-div">
       <nav className="answer-container">
         <span className="answer">{input}</span>
       </nav>
-      <main className="button-container">
         <div className="grid-button-container">
-          <Button title="7" onClick={() => clicked(7)}/>
-          <Button title="8" onClick={() => clicked(8)}/>
-          <Button title="9" onClick={() => clicked(9)}/>
-          <Button title="÷" onClick={() => clicked("÷")}/>
+          <Button title="7" onClick={() => clicked(7)} className="btn"/>
+          <Button title="8" onClick={() => clicked(8)} className="btn"/>
+          <Button title="9" onClick={() => clicked(9)} className="btn"/>
+          <Button title="÷" onClick={() => clicked("÷")} className="btn"/>
+          <Button title="=" onClick={equal} className="btn"/>
 
-          <Button title="4" onClick={() => clicked(4)}/>
-          <Button title="5" onClick={() => clicked(5)}/>
-          <Button title="6" onClick={() => clicked(6)}/>
-          <Button title="x" onClick={() => clicked("x")}/>
+          <Button title="4" onClick={() => clicked(4)} className="btn"/>
+          <Button title="5" onClick={() => clicked(5)} className="btn"/>
+          <Button title="6" onClick={() => clicked(6)} className="btn"/>
+          <Button title="x" onClick={() => clicked("x")} className="btn"/>
 
-          <Button title="1" onClick={() => clicked(1)}/>
-          <Button title="2" onClick={() => clicked(2)}/>
-          <Button title="3" onClick={() => clicked(3)}/>
-          <Button title="-" onClick={() => clicked("-")}/>
+          <Button title="1" onClick={() => clicked(1)} className="btn"/>
+          <Button title="2" onClick={() => clicked(2)} className="btn"/>
+          <Button title="3" onClick={() => clicked(3)} className="btn"/>
+          <Button title="-" onClick={() => clicked("-")} className="btn"/>
 
-          <Button title="0" onClick={() => clicked(0)}/>
-          <Button title="." onClick={() => clicked(".")}/>
-          <Button title="Del" onClick={delete_input}/>
-          <Button title="+" onClick={() => clicked("+")}/>
+          <Button title="0" onClick={() => clicked(0)} className="btn"/>
+          <Button title="." onClick={() => clicked(".")} className="btn"/>
+          <Button title="Del" onClick={delete_input} className="btn"/>
+          <Button title="+" onClick={() => clicked("+")} className="btn"/>
         </div>
-        <div className="equal-container">
-          <Button title="=" onClick={equal} className="equal"/>
-        </div>
-      </main>
     </div>
   );
 }
